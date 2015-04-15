709a045f85788ad5f7f4fe54faf386017832345e
Chris Jarvis <u7st69@gmail.com>
2015-04-15 11:43:09 +0100

    K mediod clustering
    Signed-off-by: Chris Jarvis <u7st69@gmail.com>

---
+++ prog/pamclust.r
@@ -0,0 +1,21 @@
+library(cluster)
+
+d  <- daisy(q2009)
+
+fit <- cmdscale(d,eig=TRUE, k=2) # k is the number of dim
+fit # view results
+
+#partition around mediods 5 clusters
+fit1  <- pam(d, 5)
+
+# plot solution 
+x <- fit$points[,1]
+y <- fit$points[,2]
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", 
+     main="Metric  MDS", col = fit2$clustering)
+
+
+
+
+
+
